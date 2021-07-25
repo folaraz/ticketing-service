f181c162bc8c437bc17c2a486831c3fd03d5b2f8
Olajide Abdulrazzaq Folarin <folaraz11@gmail.com>
2021-07-25 23:32:52 +0100

    initial commit

---
+++ src/test/java/com/parking/ticket/TicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
